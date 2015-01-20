de262caf3b6af67b2346aec722ab93c7e8caff16
Colin D. Munro <colin@mice-software.com>
2015-01-19 22:31:38 -0800

    Add initial virtual memory functionality

---
+++ Kernel/Kernel/mem_virtual.cpp
@@ -0,0 +1,89 @@
+#include "mem_virtual.h"
+#include "Collections.h"
+#include "StandardPC.h"
+#include "mem_logical.h"
+#include "Interrupts.h"
+#include "Process.h"
+
+static UInt32 _identifierCounter = 0;
+static KernelDictionary *s_identifierMap;
+
+static bool PageFaultHandler(void *context, void *state)
+{
+    TrapFrame *tf = (TrapFrame*)state;
+    
+}
+
+void VirtualMemory::ConfigureService(Interrupts *interruptSource)
+{
+    s_identifierMap = new KernelDictionary();
+    interruptSource->RegisterHandler(pePageFault, PageFaultHandler, NULL);
+}
+
+static UInt32 FlagForProcess(Process *process, UInt32 flags)
+{
+    return flags | ((process != NULL) ? fmUser : 0);
+}
+
+VirtualMemory::VirtualMemory(Process *process, UInt32 length)
+{
+    _identifier = _identifierCounter++;
+    _process = process;
+    _length = length;
+    _linear = PageDirectory()->Map(FlagForProcess(_process, fmNotPresent), _process ? pmApplication : pmKernel, (PhysicalPointer)(_identifier << 1), _length >> 12);
+    KernelNumber *number = new KernelNumber(_identifier);
+    s_identifierMap->Set(number, this);
+    number->Release();
+}
+
+VirtualMemory::~VirtualMemory()
+{
+    KernelNumber *number = new KernelNumber(_identifier);
+    s_identifierMap->Set(number, NULL);
+    number->Release();
+    PageDirectory()->Unmap(_linear, _length >> 12);
+}
+
+SPageDirectoryInfo* VirtualMemory::PageDirectory(void)
+{
+    return _process ? &_process->pageDirectory : &rootAddressSpace;
+}
+
+void VirtualMemory::Map(/*MAP_FLAGS*/int permissions, void *linearAddress, PhysicalPointer physicalAddress)
+{
+    if ((linearAddress < _linear) || (linearAddress >= (((char*)_linear) + _length)))
+        return; // TODO: Error!
+    PageDirectory()->Map(FlagForProcess(_process, permissions), linearAddress, physicalAddress);
+}
+
+void VirtualMemory::Unmap(void *linearAddress)
+{
+    Map(fmNotPresent, linearAddress, (PhysicalPointer)(_identifier << 1));
+}
+
+GrowableStack::GrowableStack(Process *process, UInt32 maximumPages)
+:VirtualMemory(process, (maximumPages + 1) * 4096)
+{
+    // No initialisation necessary
+}
+
+GrowableStack::~GrowableStack()
+{
+    char *base = (char*)LinearAddress();
+    for (UInt32 page = 0; page < LinearLength(); page += 4096, base += 4096) {
+        PhysicalPointer pointer = PageDirectory()->Address(base);
+        // pointer should not be CPhysicalMemory::Invalid
+        if (pointer != CPhysicalMemory::Minimum)
+            CPhysicalMemory::ReleasePages(pointer);
+    }
+}
+
+void GrowableStack::HandlePageFault(void *linearAddress)
+{
+    if (linearAddress == LinearAddress()) {
+        // Thread is experiencing a stack overflow!
+    } else {
+        PhysicalPointer newPage = CPhysicalMemory::AllocateContiguousPages();
+        Map(fmWritable, linearAddress, newPage);
+    }
+}
---
+++ Kernel/Kernel/mem_virtual.h
@@ -0,0 +1,52 @@
+#ifndef __MEM_VIRTUAL_H__
+#define __MEM_VIRTUAL_H__
+
+#include "KernelObject.h"
+#include "Collections.h"
+#include "mem_physical.h"
+
+class Process;
+class Interrupts;
+
+class VirtualMemory : public KernelObject
+{
+public:
+    static void ConfigureService(Interrupts *interruptSource);
+    
+    VirtualMemory(Process *process, UInt32 length);
+    
+    void* LinearBase(void) { return _linear; }
+    UInt32 LinearLength(void) { return _length; }
+    
+protected:
+    ~VirtualMemory();
+    
+    virtual void HandlePageFault(void *linearAddress) = 0;  // Called from ISR - if more work is necessary, place the request on a queue before handling
+    
+    // These two methods allow a virtual memory manager to add/remove a single page, be it in response to a page fault or just because they felt like it
+    void Map(/*MAP_FLAGS*/int permissions, void *linearAddress, PhysicalPointer physicalAddress);
+    void Unmap(void *linearAddress);
+    
+    SPageDirectoryInfo* PageDirectory(void);
+    
+private:
+    Process *_process;
+    UInt32 _identifier;
+    void *_linear;
+    UInt32 _length;
+};
+
+class GrowableStack : public VirtualMemory
+{
+public:
+    GrowableStack(Process *process, UInt32 maximumPages);
+    
+    void* StackTop(void) { return ((char*)LinearBase()) + LinearLength(); }
+    
+protected:
+    ~GrowableStack();
+    
+    void HandlePageFault(void *linearAddress);
+};
+
+#endif // __MEM_VIRTUAL_H__
