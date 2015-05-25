5461ade640cf20151361b872da4da22a81855a5e
Colin D. Munro <colin@mice-software.com>
2015-05-24 22:49:00 -0700

    Add target to XCode project so code insight/IDE stuff works

---
+++ Kernel/test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test
+//
+//  Created by Colin David Munro on 24/05/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
