d22927167f989ad59105f8f770326145160610a2
Colin D. Munro <colin@mice-software.com>
2016-11-19 07:55:42 -0800

    New heap that's faster

--- Kernel/Kernel/BasicHeap.cpp
+++ Kernel/Kernel/BasicHeap.cpp
@@ -11,19 +11,24 @@
 #include "runtime.h"
 #include "debug.h"
 #include "tools.h"
+#include "CPU_intr.h"
+#else
+#include <new>
 #endif
 
 #define NOTFOUND    0xFFFFFFFF
 
-#define CHECK_UNDERRUN
-#define CHECK_OVERRUN
-
-#define CLEAR_ALLOC
-#define CLEAR_FREE
+//#define CHECK_UNDERRUN
+//#define CHECK_OVERRUN
+//
+//#define CLEAR_ALLOC
+//#define CLEAR_FREE
 
 #define TEST_BEFORE 0xDEADBEEF
 #define TEST_AFTER  0xDEADF00D
 #define TEST_END    0xA110DEAD
+
+#define BYTE_ALIGN  8
 
 #ifdef TESTING
 #define HEAP_LOCK
@@ -31,150 +36,189 @@
 #define HEAP_LOCK   UninterruptableSpinLock::Autolock lock(&_lock)
 #endif
 
-// This method takes an amount and a granularity, and returns the minimum number of granules to hold the amount
-static inline BasicHeap::h_size ROUNDUP(BasicHeap::h_size amount, BasicHeap::h_size granularity)
-{
-    return (amount + (granularity - 1)) / granularity;
-}
-
-// This method takes a pointer to a base, a granularity and a number of granules, and returns a pointer to the actual address
-static inline void* OFS2PTR(void *heap, BasicHeap::h_size granularity, BasicHeap::h_size offset)
-{
-    return (void*)(BasicHeap::h_size(heap) + (granularity * offset));
-}
-
-// This method takes a pointer to a base, a granularity and a pointer into the chunk, and returns the offset granules
-static inline BasicHeap::h_size PTR2OFS(void *heap, BasicHeap::h_size granularity, void *pointer)
-{
-    return (BasicHeap::h_size(pointer) - BasicHeap::h_size(heap)) / granularity;
-}
-
-typedef struct BasicHeap_Heap {
-    BasicHeap::h_size size; // Including header/etc.
-    BasicHeap_Heap *next;
-} BasicHeap_Heap;
-
-typedef struct {
-#ifdef CHECK_UNDERRUN
-    BasicHeap::h_size _before;
+namespace Internal {
+    class Chunk {
+    public:
+        typedef enum {
+            ChunkFree,
+            ChunkAllocated,
+            ChunkEnd,
+        } ChunkType;
+    private:
+        ChunkType _type;
+    protected:
+        BasicHeap::h_size _size;
+    public:
+        Chunk(ChunkType newType, BasicHeap::h_size size)
+        :_size(size), _type(newType)
+        {
+        }
+        
+        ChunkType Type(void)
+        {
+            return _type;
+        }
+        
+        BasicHeap::h_size Size(void)
+        {
+            return _size;
+        }
+        
+        Chunk* Next(void)
+        {
+            return (Chunk*)(((char*)this) + _size);
+        }
+    };
+    
+    class Allocation : public Chunk {
+    private:
+#if defined(CLEAR_ALLOC) || defined(CLEAR_FREE)
+        void Scrub(void)
+        {
+            char *ptr = (char*)GetPointer();
+            for (BasicHeap::h_size i = _size - sizeof(Allocation); i != 0; i--, ptr++)
+                *ptr = 0;
+        }
 #endif
-    BasicHeap_Heap *base;
-    BasicHeap::h_size size;
-#ifdef CHECK_UNDERRUN
-    BasicHeap::h_size _after;
+    public:
+        Allocation(BasicHeap::h_size size)
+        :Chunk(ChunkAllocated, size)
+        {
+#ifdef CLEAR_ALLOC
+            Scrub();
 #endif
-} BasicHeap_Block;
-
-static BasicHeap::h_size FindChunk(BasicHeap_Heap *heap, BasicHeap::h_size granularity, BasicHeap::h_size count)
-{
-    const BasicHeap::h_size *data = (BasicHeap::h_size*)(heap + 1);
-    // TODO: Optimise
-    BasicHeap::h_size max = heap->size / granularity;
-    BasicHeap::h_size run = 0;
-    for (BasicHeap::h_size pos = 0; pos < max; pos++) {
-        BasicHeap::h_size index = pos / (sizeof(BasicHeap::h_size) * 8);
-        int bitNumber = pos % (sizeof(BasicHeap::h_size) * 8);
-        BasicHeap::h_size bit = 1 << bitNumber;
-        if (!(data[index] & bit))
-            run++;
-        else
-            run = 0;
-        if (run == count)
-            return pos - run + 1;
-    }
-    return NOTFOUND;
+        }
+        
+#ifdef CLEAR_FREE
+        ~Allocation()
+        {
+            Scrub();
+        }
+#endif
+        
+        static Allocation* GetObject(void *pointer)
+        {
+            return (Allocation*)(((char*)pointer) - sizeof(Allocation));
+        }
+        
+        void* GetPointer(void)
+        {
+            return ((char*)this) + sizeof(Allocation);
+        }
+    };
+    
+    class FreeSpace : public Chunk {
+    private:
+        BasicHeap *_heap;
+    public:
+        FreeSpace *last, *next;
+        
+        FreeSpace(BasicHeap *owner, BasicHeap::h_size size)
+        :Chunk(ChunkFree, size), _heap(owner)
+        {
+            last = _heap->_freeEnd;
+            next = NULL;
+            _heap->_freeEnd = this;
+            if (last)
+                last->next = this;
+            else
+                _heap->_freeStart = this;
+        }
+        ~FreeSpace()
+        {
+            if (last)
+                last->next = next;
+            if (next)
+                next->last = last;
+            if (this == _heap->_freeStart)
+                _heap->_freeStart = next;
+            if (this == _heap->_freeEnd)
+                _heap->_freeEnd = last;
+        }
+        
+        FreeSpace* Split(BasicHeap::h_size size)
+        {
+            if (_size < (sizeof(FreeSpace) * 2))
+                return this;
+            if (size < sizeof(FreeSpace))
+                size = sizeof(FreeSpace);
+            size = (size + (BYTE_ALIGN - 1)) & -BYTE_ALIGN;   // Some sort of alignment
+            
+            BasicHeap *owner = _heap;
+            BasicHeap::h_size total = _size;
+            char *base = (char*)this;
+            
+            this->~FreeSpace();
+            
+            new (base + size) FreeSpace(owner, total - size);
+            return new (base) FreeSpace(owner, size);
+        }
+        
+        void Merge(void)
+        {
+            BasicHeap *heap = _heap;
+            FreeSpace *freespace = this;
+            while (true) {
+                void *address = freespace;
+                Internal::FreeSpace *next = (Internal::FreeSpace*)freespace->Next();
+                if (next->Type() != Internal::Chunk::ChunkFree)
+                    break;
+                BasicHeap::h_size total = freespace->Size();
+                freespace->~FreeSpace();
+                total += next->Size();
+                next->~FreeSpace();
+                freespace = new (address) FreeSpace(heap, total);
+            }
+        }
+    };
 }
 
-static void SetChunk(BasicHeap_Heap *heap, BasicHeap::h_size base, BasicHeap::h_size count, bool used)
+BasicHeap::BasicHeap()
 {
-    BasicHeap::h_size *data = (BasicHeap::h_size*)(heap + 1);
-    // TODO: Optimise
-    while (count != 0) {
-        BasicHeap::h_size index = base / (sizeof(BasicHeap::h_size) * 8);
-        int bitNumber = base % (sizeof(BasicHeap::h_size) * 8);
-        BasicHeap::h_size bit = 1 << bitNumber;
-        if (used)
-            data[index] |= bit;
-        else
-            data[index] &= ~bit;
-        count--;
-        base++;
-    }
-}
-
-BasicHeap::BasicHeap(BasicHeap::h_size gran)
-{
-    _root = NULL;
+    _freeStart = _freeEnd = NULL;
     _total = 0;
-    _allocated = 0;
+    _used = 0;
     _count = 0;
-    _granularity = gran;
 }
 
 BasicHeap::~BasicHeap()
 {
-    if (_count != 0) {
-        // TODO: Error! All of the memory allocated by this heap is about to vanish
-    }
 }
 
 void BasicHeap::AddBlock(void *offset, BasicHeap::h_size length)
 {
-    HEAP_LOCK;
+    new (((char*)offset) + length - sizeof(Internal::Chunk)) Internal::Chunk(Internal::Chunk::ChunkEnd, sizeof(Internal::Chunk));
+    new (offset) Internal::FreeSpace(this, length - sizeof(Internal::Chunk));
     _total += length;
-    BasicHeap_Heap *heap = (BasicHeap_Heap*)offset;
-    heap->size = length;
-    h_size *data = (h_size*)(heap + 1);
-    h_size bitFields = ROUNDUP(ROUNDUP(length, _granularity), sizeof(h_size) * 8);
-    for (h_size count = bitFields; count != 0; count--, data++)
-        *data = 0;
-    SetChunk(heap, 0, ROUNDUP(sizeof(BasicHeap_Heap) + (sizeof(h_size) * bitFields), _granularity), true);
-    heap->next = (BasicHeap_Heap*)_root;
-    _root = heap;
 }
 
 void* BasicHeap::Alloc(BasicHeap::h_size amount)
 {
     HEAP_LOCK;
-    h_size required = amount + sizeof(BasicHeap_Block);
-#ifdef CHECK_OVERRUN
-    required += sizeof(BasicHeap::h_size);
-#endif
-    h_size count = ROUNDUP(required, _granularity);
-    for (BasicHeap_Heap *heap = (BasicHeap_Heap*)_root; heap != NULL; heap = heap->next) {
-        h_size offset = FindChunk(heap, _granularity, count);
-        if (offset != NOTFOUND) {
-            SetChunk(heap, offset, count, true);
-            BasicHeap_Block *block = (BasicHeap_Block*)OFS2PTR(heap, _granularity, offset);
-            block->base = heap;
-            block->size = amount;
-            _count++;
-            _allocated += count * _granularity;
-#ifdef CHECK_UNDERRUN
-            block->_before = TEST_BEFORE;
-            block->_after = TEST_AFTER;
-#endif
-#ifdef CLEAR_ALLOC
-            char *blockStart = (char*)(block + 1);
-            for (h_size length = (count * _granularity) - sizeof(BasicHeap_Block); length != 0; length--, blockStart++)
-                *blockStart = length % 0xFF;
-#endif
-#ifdef CHECK_OVERRUN
-            char *temp = (char*)(block + 1);
-            temp += amount;
-            ((BasicHeap::h_size*)temp)[0] = TEST_END;
-#endif
-            return block + 1;
+    Internal::FreeSpace *freespace;
+    BasicHeap::h_size totalAmount = amount + sizeof(Internal::Allocation);
+    for (freespace = _freeStart; freespace != NULL; freespace = freespace->next) {
+        freespace->Merge(); // Just in case
+        BasicHeap::h_size available = freespace->Size() - sizeof(Internal::Allocation);
+        if (available >= totalAmount) {
+            if (available > (totalAmount + 30))
+                freespace = freespace->Split(totalAmount);
+            break;
         }
     }
-    kprintf("Allocation failure for %i\n", amount);
-    Test();
-    return NULL;
+    if (!freespace)
+        return NULL;
+    totalAmount = freespace->Size();    // You might be getting more than you asked for
+    freespace->~FreeSpace();
+    _used += totalAmount;
+    _count++;
+    Internal::Allocation *allocation = new (freespace) Internal::Allocation(totalAmount);
+    return allocation->GetPointer();
 }
 
 static void HeapDebug(const char *error)
 {
-    kprintf(error);
+    //    kprintf(error);
 #ifdef TESTING
     throw "Heap integrity failure";
 #endif
@@ -183,32 +227,13 @@ static void HeapDebug(const char *error)
 void BasicHeap::Release(void *buffer)
 {
     HEAP_LOCK;
-    BasicHeap_Block *block = ((BasicHeap_Block*)buffer) - 1;
-#ifdef CHECK_UNDERRUN
-    if (block->_before != TEST_BEFORE)
-        HeapDebug("HEAP: _before invalid\n");
-    if (block->_after != TEST_AFTER)
-        HeapDebug("HEAP: _after invalid\n");
-#endif
-    h_size required = block->size + sizeof(BasicHeap_Block);
-#ifdef CHECK_OVERRUN
-    char *temp = (char*)(block + 1);
-    temp += block->size;
-    if (((BasicHeap::h_size*)temp)[0] != TEST_END)
-        HeapDebug("HEAP: overrun invalid\n");
-    required += sizeof(BasicHeap::h_size);
-#endif
-    h_size count = ROUNDUP(required, _granularity);
-    h_size offset = PTR2OFS(block->base, _granularity, block);
-    SetChunk(block->base, offset, count, false);
+    Internal::Allocation *allocation = Internal::Allocation::GetObject(buffer);
+    BasicHeap::h_size size = allocation->Size();
+    allocation->~Allocation();
+    _used -= size;
     _count--;
-    _allocated -= count * _granularity;
-#ifdef CLEAR_FREE
-    char *blockStart = (char*)block;
-    for (h_size amount = count * _granularity; amount != 0; amount--, blockStart++) {
-        *blockStart = amount & 1 ? 0xFF : 0x55; // 0xFF55FF55
-    }
-#endif
+    Internal::FreeSpace *freespace = new (allocation) Internal::FreeSpace(this, size);
+    freespace->Merge();
 }
 
 BasicHeap::h_size BasicHeap::TotalMemory(void)
@@ -218,12 +243,7 @@ BasicHeap::h_size BasicHeap::TotalMemory(void)
 
 BasicHeap::h_size BasicHeap::UsedMemory(void)
 {
-    return _allocated;
-}
-
-BasicHeap::h_size BasicHeap::FreeMemory(void)
-{
-    return _total - _allocated;
+    return _used;
 }
 
 BasicHeap::h_size BasicHeap::AllocationCount(void)
@@ -233,55 +253,16 @@ BasicHeap::h_size BasicHeap::AllocationCount(void)
 
 void BasicHeap::Test(void)
 {
-    kprintf("Heaps:\n");
-    int heapNo = 0;
-    BasicHeap::h_size totalSize = 0;
-    for (BasicHeap_Heap *heap = (BasicHeap_Heap*)_root; heap != NULL; heap = heap->next) {
-        const BasicHeap::h_size *data = (BasicHeap::h_size*)(heap + 1);
-        BasicHeap::h_size max = heap->size / _granularity;
-        BasicHeap::h_size run = 0;
-        BasicHeap::h_size total = 0;
-        BasicHeap::h_size longestRun = 0;
-        for (BasicHeap::h_size pos = 0; pos < max; pos++) {
-            BasicHeap::h_size index = pos / (sizeof(BasicHeap::h_size) * 8);
-            int bitNumber = pos % (sizeof(BasicHeap::h_size) * 8);
-            BasicHeap::h_size bit = 1 << bitNumber;
-            if (!(data[index] & bit)) {
-                total++;
-                run++;
-            } else {
-                if (run > longestRun)
-                    longestRun = run;
-                run = 0;
-            }
-        }
-        if (run > longestRun)
-            longestRun = run;
-        int freeBytes = total * _granularity;
-        int freeLongest = longestRun * _granularity;
-        int amount = (10000*(max - total)) / max;
-        int major = amount / 100;
-        int minor = amount % 100;
-        kprintf("\t%i: %i total, %i bytes free, %i longest contiguous bytes free, %i.%.2i%% utilisation\n", heapNo, (int)heap->size, freeBytes, freeLongest, major, minor);
-        heapNo++;
-        totalSize += total;
+    kprintf("%i total, %i used in %i allocations\n", _total, _used, _count);
+    kprintf("Free: ");
+    for (Internal::FreeSpace *freespace = _freeStart; freespace != NULL; freespace = freespace->next) {
+        kprintf("%i; ", freespace->Size());
+        if (freespace->Next()->Type() == Internal::Allocation::ChunkFree)
+            kprintf("*");
     }
-    kprintf("\tTotal in heaps: %i bytes\n", totalSize * _granularity);
+    kprintf("\n");
 }
 
 void BasicHeap::Check(void *pointer)
 {
-    BasicHeap_Block *block = ((BasicHeap_Block*)pointer) - 1;
-#ifdef CHECK_UNDERRUN
-    if (block->_before != TEST_BEFORE)
-        HeapDebug("HEAP: _before invalid\n");
-    if (block->_after != TEST_AFTER)
-        HeapDebug("HEAP: _after invalid\n");
-#endif
-#ifdef CHECK_OVERRUN
-    char *temp = (char*)(block + 1);
-    temp += block->size;
-    if (((BasicHeap::h_size*)temp)[0] != TEST_END)
-        HeapDebug("HEAP: overrun invalid\n");
-#endif
 }
--- Kernel/Kernel/BasicHeap.h
+++ Kernel/Kernel/BasicHeap.h
@@ -13,13 +13,17 @@
 #include "Locking.h"
 #endif
 
+namespace Internal {
+    class FreeSpace;
+}
+
 class BasicHeap
 {
 public:
     typedef unsigned long h_size;
 
 public:
-    BasicHeap(h_size gran = 8192);
+    BasicHeap();
     ~BasicHeap();
     
     // Management
@@ -41,8 +45,10 @@ private:
 #ifndef TESTING
     UninterruptableSpinLock _lock;
 #endif
-    void *_root;
-    h_size _total, _allocated, _count, _granularity;
+    friend class Internal::FreeSpace;
+    Internal::FreeSpace *_freeStart, *_freeEnd;
+    
+    h_size _total, _used, _count;
 };
 
 #endif /* defined(__MemoryAllocTest__BasicHeap__) */
